dae5d3d3ad3281a01907c82e1dfc145d7d6c6a8a
Lisseth00 <lissethrivas1328@gmail.com>
2025-05-09 18:20:37 -0500

    Se agrego la ruta para una nueva tarea

---
+++ src/Public/js/newTarea.js
@@ -0,0 +1,47 @@
+const { $where } = require("../Models/tarea");
+
+const btn = document.getElementById('btn');
+
+
+
+$(document).ready(function(){    
+    $('#alert').hide();
+});
+
+$('#btn-alert').click(function(){
+    $('#alert').hide();
+}); 
+
+
+btn.onclick = () => {
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+
+
+    if (title.length == '' || description.length == '') {
+         $('#alert').show();
+
+    } else {
+
+        //guaradar datos en la bdd
+        postData(title, description);
+
+    }
+}
+
+async function postData(title, description) {
+    const response = await fetch('/api/save', {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            description: description
+        })
+
+    });
+    const data = await response.json();
+    console.data(data);
+}
