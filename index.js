09c66e56a37fd2f281238c90fc7c8251a1ff8af9
Deyson Urrego <deysonurrego159@gmail.com>
2025-05-17 11:35:42 -0500

    implemente el llamado a la base de datos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('./src/database/conexion');
 const path = require('path');
 
 const app = express();
